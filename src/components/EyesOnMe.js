cfa733e84c5001e7d51800eb8e99250628fbd11c
Will Bush <williamdcbush@gmail.com>
2019-10-28 14:35:02 +0000

    Done.

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,27 @@
 // Code EyesOnMe Component Here
+import React from 'react';
+
+class EyesOnMe extends React.Component {
+
+    constructor(props) {
+        super()
+        // this.state = {
+        //     key: this.props.attribute
+        // }
+    }
+
+    handleOnFocus = () => console.log('Good!')
+
+    handleOnBlur = () => console.log('Hey! Eyes on me!')
+
+    render() {
+
+        return (
+            <div>
+                <button onFocus={this.handleOnFocus} onBlur={this.handleOnBlur} ></button>
+            </div>
+        )
+    }
+}
+
+export default EyesOnMe
